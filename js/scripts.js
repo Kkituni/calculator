5956dbfa331aaa4a09d5db5e139fe5f1612c02cb
Kelvin <@kkituni@gmail.comgit config --global user.email @kkituni@gmail.com>
2019-03-19 15:51:52 +0300

    Added commands to the html and js pages for the calculator to operate

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function subtraction(a,c){
+  return a-c;
+}
+var a = parseInt(prompt("Enter the first number: "));
+var c = parseInt(prompt("Enter the second number: "));
+var result = a-c;
+alert(result)
